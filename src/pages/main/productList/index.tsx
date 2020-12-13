93df13975b285ee46b70f1cd94319b779270f975
masaditya <aeka489@gmail.com>
2020-12-14 01:14:20 +0700

    add filter

--- src/pages/main/productList/index.tsx
+++ src/pages/main/productList/index.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { BackTop, Button, Layout, Menu, Radio, Row } from "antd";
-import SubMenu from "antd/lib/menu/SubMenu";
+import { BackTop, Button, Layout, Menu, Radio, Row, Space } from "antd";
 import { ArrowUpOutlined, SkinOutlined, UserOutlined } from "@ant-design/icons";
-import { useHistory } from "react-router-dom";
 import { useProductStore } from "../../../lib/store";
 import { TProduct } from "../../../types";
 import { Product } from "../../../components/product";
@@ -10,22 +8,11 @@ import { useProductService } from "../../../lib/hook/service";
 
 export const ProductList = () => {
   const { getProducts } = useProductService();
-  const [maleProducts, setmaleProducts] = useState<TProduct[]>([]);
-  const [femaleProduct, setFemaleProduct] = useState<TProduct[]>([]);
-
-  const [topsProduct, setTopsProduct] = useState<TProduct[]>([]);
-  const [bottomsProduct, setBottomsProduct] = useState<TProduct[]>([]);
-
   const [sexFilter, setSexFilter] = useState();
   const [typeFilter, setTypeFilter] = useState();
-  const {
-    products,
-    womenProduct,
-    menProduct,
-    setProducts,
-    setWomenProduct,
-    setMenProduct,
-  } = useProductStore((state) => state);
+  const { products, setProducts } = useProductStore((state) => state);
+  const [showedProducts, setShowedProducts] = useState<TProduct[]>(products);
+
   const { Sider, Content } = Layout;
 
   const getProductCall = useCallback(async () => {
@@ -34,7 +21,7 @@ export const ProductList = () => {
       console.log(response);
       if (response.data) {
         setProducts(response.data);
-        fillSegmentProduct(response.data);
+        setShowedProducts(response.data);
       }
     } catch (error) {
       console.log(error);
@@ -42,47 +29,51 @@ export const ProductList = () => {
     }
   }, []);
 
-  const fillSegmentProduct = useCallback(
-    (data: TProduct[]) => {
-      let fwomenProduct: TProduct[] = data.filter((value: TProduct) => {
-        return value.gender === "female";
-      });
-      setFemaleProduct(fwomenProduct);
-      setWomenProduct(fwomenProduct);
-      let fmenProduct: TProduct[] = data.filter((value: TProduct) => {
-        return value.gender === "male";
-      });
-      setmaleProducts(fmenProduct);
-      setMenProduct(fmenProduct);
-      let tops: TProduct[] = data.filter((value: TProduct) => {
-        return value.category === "tops";
-      });
-      setTopsProduct(tops);
-      let bottoms: TProduct[] = data.filter((value: TProduct) => {
-        return value.category === "bottoms";
-      });
-      setTopsProduct(bottoms);
-    },
-    [products]
-  );
-
   useEffect(() => {
     if (products === undefined || products.length == 0) {
       getProductCall();
     } else {
-      fillSegmentProduct(products);
+      setShowedProducts(products);
     }
   }, []);
 
-  const applyTypeFilter = useCallback((e: any) => {
-    console.log(e);
-    setTypeFilter(e.target.value);
-  }, []);
+  const applyTypeFilter = useCallback(
+    (e: any) => {
+      console.log(e);
+      setTypeFilter(e.target.value);
+      let tmp: TProduct[];
+      if (sexFilter) {
+        tmp = products.filter((item) => {
+          return item.category === e.target.value && item.gender === sexFilter;
+        });
+      } else {
+        tmp = products.filter((item) => {
+          return item.category === e.target.value;
+        });
+      }
+      setShowedProducts(tmp);
+    },
+    [showedProducts]
+  );
 
-  const applySexFilter = useCallback((e: any) => {
-    console.log(e);
-    setSexFilter(e.target.value);
-  }, []);
+  const applySexFilter = useCallback(
+    (e: any) => {
+      console.log(e);
+      setSexFilter(e.target.value);
+      let tmp: TProduct[];
+      if (typeFilter) {
+        tmp = products.filter((item) => {
+          return item.gender === e.target.value && item.category === typeFilter;
+        });
+      } else {
+        tmp = products.filter((item) => {
+          return item.gender === e.target.value;
+        });
+      }
+      setShowedProducts(tmp);
+    },
+    [showedProducts]
+  );
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -102,8 +93,10 @@ export const ProductList = () => {
         }}
       >
         <Menu mode="inline" style={{ height: "100%", borderRight: 0 }}>
-          <Menu.Item style={{ fontWeight: "bold" }}> Filter </Menu.Item>
-          <SubMenu key="sub1" icon={<SkinOutlined />} title="Type">
+          <Menu.Item disabled style={{ fontWeight: "bold" }}>
+            Filter
+          </Menu.Item>
+          <Menu.SubMenu key="sub1" icon={<SkinOutlined />} title="Type">
             <Radio.Group onChange={applyTypeFilter} value={typeFilter || ""}>
               <Radio value="tops" className="px-2 mv-1">
                 Tops
@@ -112,8 +105,8 @@ export const ProductList = () => {
                 Bottoms
               </Radio>
             </Radio.Group>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<UserOutlined />} title="Sex">
+          </Menu.SubMenu>
+          <Menu.SubMenu key="sub2" icon={<UserOutlined />} title="Sex">
             <Radio.Group onChange={applySexFilter} value={sexFilter || ""}>
               <Radio value="male" className="px-2 mv-1">
                 Male
@@ -122,12 +115,24 @@ export const ProductList = () => {
                 Female
               </Radio>
             </Radio.Group>
-          </SubMenu>
+          </Menu.SubMenu>
+          <Menu.Item
+            onClick={() => {
+              setSexFilter(undefined);
+              setTypeFilter(undefined);
+              setShowedProducts(products);
+            }}
+            active={false}
+            style={{ fontSize: "0.8em" }}
+          >
+            {" "}
+            Clear Filter{" "}
+          </Menu.Item>
         </Menu>
       </Sider>
       <Content>
         <Row style={{ marginTop: 50 }} justify="space-around">
-          {products.map((item: TProduct) => {
+          {showedProducts.map((item: TProduct) => {
             return (
               <div className="mb-2" key={item.id_product}>
                 <Product {...item} />
